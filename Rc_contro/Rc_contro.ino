d93abb905e83f0ac265c26e3133a60f6ad52861e
UkritFB <kobori4268@gmail.com>
2018-08-27 16:58:03 +0700

    v1.1.2

--- Rc_contro/Rc_contro.ino
+++ Rc_contro/Rc_contro.ino
@@ -1,28 +1,24 @@
-
-
-
-
 #include "Offset.h"
 //#################### Config Pin Motor Driver #########################
 // สำหรับการ Set Pin Drive Motor
-int PIN_ENA  = 0;
-int PIN_ENB  = 0;
-int PIN_L1   = 0;
-int PIN_L2   = 0;
-int PIN_R1   = 0;
-int PIN_R2   = 0;
-//####################### Config Pin Recive ############################
+int PIN_ENA  = 9;
+int PIN_ENB  = 11;
+int PIN_L1   = 7;
+int PIN_L2   = 8;
+int PIN_R1   = 12;
+int PIN_R2   = 13;
+//###################### Config Pin Recive ############################
 // สำหรับการ Set Pin Reciver
-int PIN_CH1  = 0;
-int PIN_CH2  = 0;
-int PIN_CH3  = 0;
-int PIN_CH4  = 0;
+int PIN_CH1  = A1;
+int PIN_CH2  = A2;
+int PIN_CH3  = A3;
+int PIN_CH4  = A4;
 // ---------------------- offset Recive -------------------------------
 // สัญญานจาก Remote ที่ผ่านการ Offset แล้ว
-int DATA_CH1 = 0;
-int DATA_CH2 = 0;
-int DATA_CH3 = 0;
-int DATA_CH4 = 0;
+int DATA_CH1 = 0; // L  |
+int DATA_CH2 = 0; // L  _
+int DATA_CH3 = 0; // R  |
+int DATA_CH4 = 0; // R  _
 //####################### Config Pin Attack ############################
 // สำหรับ การ Set Pin Relay
 int PIN_Att = 0;
@@ -30,6 +26,17 @@ int PIN_Att = 0;
 int signal_A4, signal_A1, signal_A2, signal_A3;
 int speed_go, speed_bk;
 //######################################################################
+
+
+
+
+
+
+
+
+
+
+
 
 //**********************************************************************
 void setup() {
@@ -44,12 +51,39 @@ void setup() {
   pinMode(PIN_ENA, OUTPUT);
   pinMode(PIN_ENB, OUTPUT);
   Serial.begin(9600);
-  
+
 }
 //**********************************************************************
 void loop() {
+  read_rc();
+  if (DATA_CH1 <= 40 && DATA_CH1 >= 60) {       //กรณีที่โยก Pit | เพียงอย่างเดียว จะเดินหน้ากรือถอยหลัง
+    
+  }else if(DATA_CH2 <= 40 && DATA_CH2 >= 60){   //กรณีที่โยก Pit - เพียงอย่างเดียว จะเลียวซ้ายหรือเลี้ยวขวา
+    
+  }
+  else{   // ในกรณีที่ pit X หรือ /   \  
+    
+  }
 }
 //**********************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //#####################################################################
 void read_rc() {
   DATA_CH1 = offset_CH(PIN_CH1, 1130, 1950);
@@ -79,6 +113,11 @@ void M_L_B(int SP_M) {
   digitalWrite(PIN_L2, 1);
   analogWrite(PIN_ENA, SP_M);
 }
+void M_L_S() {
+  digitalWrite(PIN_L1, 0);
+  digitalWrite(PIN_L2, 0);
+  analogWrite(PIN_ENA, 0);
+}
 void M_R_G(int SP_M) {
   digitalWrite(PIN_R1, 1);
   digitalWrite(PIN_R2, 0);
@@ -89,37 +128,31 @@ void M_R_B(int SP_M) {
   digitalWrite(PIN_R2, 1);
   analogWrite(PIN_ENB, SP_M);
 }
+void M_R_S() {
+  digitalWrite(PIN_R1, 0);
+  digitalWrite(PIN_R2, 0);
+  analogWrite(PIN_ENB, 0);
+}
 //#####################################################################
 void go(int SP_M) {
   M_L_G(SP_M);
   M_R_G(SP_M);
 }
-
 void back(int SP_M) {
   M_L_B(SP_M);
   M_R_B(SP_M);
 }
-
-void L() {
-  digitalWrite(7, 1);
-  digitalWrite(8, 0);
-  analogWrite(9, 255);
+void L(int SP_M) {
+  M_R_G(SP_M);
 }
-void R()
+void R( int SP_M)
 {
-  digitalWrite(13, 1);
-  digitalWrite(12, 0);
-  analogWrite(11, 255);
+  M_L_G(SP_M);
 }
-void B()
-{
-  digitalWrite(7, 0);
-  digitalWrite(8, 0);
-  analogWrite(9, 0);
-
-  digitalWrite(13, 0);
-  digitalWrite(12, 0);
-  analogWrite(11, 0);
+void B() {
+  M_L_S();
+  M_R_S();
 }
+//#####################################################################
 
 
